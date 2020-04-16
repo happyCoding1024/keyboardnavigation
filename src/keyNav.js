cd8fcc6917c8cd850e390cb5ace43bc7fbda7aa1
happyCoding1024 <zhangguicheng_1995@163.com>
2020-04-16 23:06:54 +0800

    basic function done

---
+++ src/keyNav.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from 'react';
+import genKeyboard from './genKeyboard';
+import listenUser from './listenUser';
+import handleFocus from './handleFocus';
+import { KbdNavWrapper, KbdNav, KbdNavdesc } from './style/style';
+import { GlobalResetStyle } from './style/reset';
+
+class KeyboardNavigation extends Component {
+ 
+  render() {
+    return (
+      <Fragment>
+        <GlobalResetStyle />
+        <KbdNavWrapper>
+          <KbdNavdesc className='KdbNavDesc'>
+            <h1>键盘导航</h1>
+            <p>Alt+Q关闭键盘导航,Alt+R开启键盘导航，自定义原始键盘映射，输入时自动屏蔽键盘导航</p>
+          </KbdNavdesc>
+          <KbdNav className='kbdNav' ref={(KbdNav) => {this.KbdNavElem=KbdNav}}></KbdNav> 
+        </KbdNavWrapper>
+      </Fragment>
+    )
+  }
+
+  componentDidMount() {
+    this.hash = genKeyboard(this.KbdNavElem);
+    // 刚打开页面时，要能监听用户的按键。下面的componentDidUpdate要在更新数据之后才会响应。
+    listenUser(this.hash, false)
+    const { inputElemArr } = this.props;
+    // Ctrl + q 停用键盘导航
+    document.addEventListener('keydown', (event) => {
+      const keyCode = event.keyCode || event.which || event.charCode;
+      const altKey = event.altKey || event.metaKey;
+      console.log('key = ', keyCode)
+      if (altKey && keyCode === 81) {
+        if (!(altKey && keyCode === 82)) {
+          alert('您已停用键盘导航，按alt+r可重新开启键盘导航');
+          listenUser(this.hash, true)
+        }
+      } else if (altKey && keyCode === 82) {
+          alert('您已开启键盘导航，按alt+p可关闭键盘导航');
+          listenUser(this.hash, false)          
+          handleFocus(inputElemArr, this.hash);
+      }
+    })
+  }
+
+  componentDidUpdate() {
+    const { inputElemArr } = this.props;
+    console.log('inputELemArr', inputElemArr);
+    // Ctrl + q 停用键盘导航
+    document.addEventListener('keydown', (event) => {
+      const keyCode = event.keyCode || event.which || event.charCode;
+      const altKey = event.altKey || event.metaKey;
+      console.log('key = ', keyCode)
+      if (altKey && keyCode === 81) {
+        if (!(altKey && keyCode === 82)) {
+          alert('您已停用键盘导航，按alt+r可重新开启键盘导航');
+          listenUser(this.hash, true)
+        }
+      } else if (altKey && keyCode === 82) {
+          alert('您已开启键盘导航，按alt+p可关闭键盘导航');
+          listenUser(this.hash, false)          
+          handleFocus(inputElemArr, this.hash);
+      }
+    })
+  }
+
+}
+
+export default KeyboardNavigation;
