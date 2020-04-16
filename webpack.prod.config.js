85d93be779d1b0cdeff6cfb71d2d1d94e86b1135
happyCoding1024 <zhangguicheng_1995@163.com>
2020-04-16 23:00:59 +0800

    npm bundle done

---
+++ webpack.prod.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/keyNav.js'
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin({
+      dry: false,
+      cleanStaleWebpackAssets: true,
+      cleanOnceBeforeBuildPatterns: [
+        path.resolve(__dirname, 'dist'),
+        "**/*"
+      ]
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'keyboardnavigation'
+  },
+  externals: [nodeExternals()]
+}
