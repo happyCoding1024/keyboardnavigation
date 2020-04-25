75431767aa38176493df95d3e2f2ea0cf0f6343b
happyCoding1024 <zhangguicheng_1995@163.com>
2020-04-25 16:54:30 +0800

    remove HtmlWebpackPlugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
